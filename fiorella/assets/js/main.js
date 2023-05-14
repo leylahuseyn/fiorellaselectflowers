22d910a25559c35211431bc79e2bb86a4bd980a6
leylahuseyn <127212987+leylahuseyn@users.noreply.github.com>
2023-05-14 11:24:33 +0400

    Add files via upload

---
+++ fiorella/assets/js/main.js
@@ -0,0 +1,11 @@
+const images = document.querySelectorAll("flwr")
+const menu = document.querySelectorAll(".menu .select")
+const atribute = div.getAttribute('atribute');
+function selectFlowers(){
+    if(!(div.getAttribute('class').includes('atribute'))){
+        div.style.display='block'
+    }
+    else{
+        div.style.display='none'
+    }
+}
